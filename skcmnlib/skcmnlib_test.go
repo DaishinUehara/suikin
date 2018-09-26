e4aa518c0de988a69ca56edd7719c2a75d844268
DaishinUehara <git.uehara@gmail.com>
2018-09-27 00:17:05 +0900

    テストパターン追加

--- skcmnlib/skcmnlib_test.go
+++ skcmnlib/skcmnlib_test.go
@@ -7,22 +7,55 @@ import (
 )
 
 func TestCammaDivide(t *testing.T) {
-	selectColumnName := make([]string, 0, 5)
-	selectColumnName = append(selectColumnName, "工場")
-	selectColumnName = append(selectColumnName, "売上")
-	selectColumnName = append(selectColumnName, "原価")
-	selectColumnName = append(selectColumnName, "売上総利益")
+	selectColumnName1 := make([]string, 0, 5)
+	selectColumnName1 = append(selectColumnName1, "工場")
+	selectColumnName1 = append(selectColumnName1, "売上")
+	selectColumnName1 = append(selectColumnName1, "原価")
+	selectColumnName1 = append(selectColumnName1, "売上総利益")
 
-	incolumnname, outcolumnname, err := skcmnlib.CammaDivide(selectColumnName)
+	incolumnname1, outcolumnname1, err1 := skcmnlib.CammaDivide(selectColumnName1)
 
-	if err != nil {
-		t.Errorf("skcmnlib.CammaDivide:想定できないエラー: %v", err)
+	if err1 != nil {
+		t.Errorf("skcmnlib.CammaDivide:想定できないエラー: %v", err1)
 	}
 
-	for i, ans := range selectColumnName {
-		if incolumnname[i] != ans {
-			t.Errorf("skcmnlib.CammaDivide:incolumnname[%d]=[戻値:%s]:[想定:%s]\n", i, incolumnname[i], ans)
-			t.Errorf("skcmnlib.CammaDivide:outcolumnname[%d]=[戻値:%s]:[想定:%s]\n", i, outcolumnname[i], ans)
+	for i, ans1 := range selectColumnName1 {
+		if incolumnname1[i] != ans1 {
+			t.Errorf("skcmnlib.CammaDivide:incolumnname1[%d]=[戻値:%s]:[想定:%s]\n", i, incolumnname1[i], ans1)
+			t.Errorf("skcmnlib.CammaDivide:outcolumnname1[%d]=[戻値:%s]:[想定:%s]\n", i, outcolumnname1[i], ans1)
+		}
+	}
+
+	selectColumnName2 := make([]string, 0, 5)
+	selectColumnName2 = append(selectColumnName2, "工場,工場1")
+	selectColumnName2 = append(selectColumnName2, "売上,売上(合計)")
+	selectColumnName2 = append(selectColumnName2, "原価,原価(合計)")
+	selectColumnName2 = append(selectColumnName2, "粗利,粗利(合計)")
+
+	incolumnname2, outcolumnname2, err2 := skcmnlib.CammaDivide(selectColumnName2)
+
+	if err2 != nil {
+		t.Errorf("skcmnlib.CammaDivide:想定できないエラー: %v", err2)
+	}
+
+	inans2 := make([]string, 0, 5)
+	inans2 = append(inans2, "工場")
+	inans2 = append(inans2, "売上")
+	inans2 = append(inans2, "原価")
+	inans2 = append(inans2, "粗利")
+
+	outans2 := make([]string, 0, 5)
+	outans2 = append(outans2, "工場1")
+	outans2 = append(outans2, "売上(合計)")
+	outans2 = append(outans2, "原価(合計)")
+	outans2 = append(outans2, "粗利(合計)")
+
+	for i, input := range selectColumnName2 {
+		if incolumnname2[i] != inans2[i] {
+			t.Errorf("skcmnlib.CammaDivide:incolumnname1[%d]=[入力値:%s]:[戻値:%s]:[想定:%s]\n", i, input, incolumnname2[i], inans2)
+		}
+		if outcolumnname2[i] != outans2[i] {
+			t.Errorf("skcmnlib.CammaDivide:outcolumnname1[%d]=[入力値:%s]:[戻値:%s]:[想定:%s]\n", i, input, outcolumnname2[i], outans2)
 		}
 	}
 }
