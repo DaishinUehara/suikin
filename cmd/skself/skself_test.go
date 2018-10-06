a5040da01adc979b795be03451049a8fd30445a4
DaishinUehara <git.uehara@gmail.com>
2018-10-06 19:19:05 +0900

    add test for skself

--- cmd/skself/skself_test.go
+++ cmd/skself/skself_test.go
@@ -104,4 +104,33 @@ func TestSelExec(t *testing.T) {
 	} else {
 		t.Errorf("[NG]:main.selfExec(%v):err=%v,stdin=%v,stdout=%v,stderr=%v\n", argv, err, strStdIn, strStdOut, strStdErr)
 	}
+
+	argv = make([]string, 0, 4)
+	argv = append(argv, "skself")
+	argv = append(argv, "-")
+	argv = append(argv, "-")
+	argv = append(argv, "-")
+	argv = append(argv, "項目1")
+	strStdIn = "項目1"
+	strStdOut, strStdErr, err = skstublib.SkStdStub(strStdIn, argv, func(argv1 []string) error { err1 := selfExec(argv1); return err1 })
+	if err == nil && strStdOut == "項目1\n" && strStdErr == "" {
+		t.Logf("[OK]:main.selfExec(%v):err=%v,stdin=%v,stdout=%v,stderr=%v\n", argv, err, strStdIn, strStdOut, strStdErr)
+	} else {
+		t.Errorf("[NG]:main.selfExec(%v):err=%v,stdin=%v,stdout=%v,stderr=%v\n", argv, err, strStdIn, strStdOut, strStdErr)
+	}
+
+	argv = make([]string, 0, 4)
+	argv = append(argv, "skself")
+	argv = append(argv, "-")
+	argv = append(argv, "-")
+	argv = append(argv, "-")
+	argv = append(argv, "項目3,c")
+	argv = append(argv, "項目1")
+	strStdIn = "項目1 項目2 項目3\n1 2 3\n4 5 6"
+	strStdOut, strStdErr, err = skstublib.SkStdStub(strStdIn, argv, func(argv1 []string) error { err1 := selfExec(argv1); return err1 })
+	if err == nil && strStdOut == "c 項目1\n3 1\n6 4\n" && strStdErr == "" {
+		t.Logf("[OK]:main.selfExec(%v):err=%v,stdin=%v,stdout=%v,stderr=%v\n", argv, err, strStdIn, strStdOut, strStdErr)
+	} else {
+		t.Errorf("[NG]:main.selfExec(%v):err=%v,stdin=%v,stdout=%v,stderr=%v\n", argv, err, strStdIn, strStdOut, strStdErr)
+	}
 }
