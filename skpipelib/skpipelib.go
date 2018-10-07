5e97d1692427600c0033ff14ace8b005f5601c58
DaishinUehara <git.uehara@gmail.com>
2018-10-07 23:44:20 +0900

    add SkPipe.AddExec Method.

--- skpipelib/skpipelib.go
+++ skpipelib/skpipelib.go
@@ -9,10 +9,24 @@ type SkExecIf interface {
 	Exec(io.Reader, io.Writer, io.Writer, []string, []string) error
 }
 
-// SkPipeIF Pipeへのアクセスインターフェース
+// SkPipeIf Pipeへのアクセスインターフェース
 type SkPipeIf interface {
 	GetStdin() (io.Writer, error)
 	AddExec(SkExecIf) error
 	GetStdout() io.Reader
 	GetStderr() io.Reader
+	Exec(io.Reader, io.Writer, io.Writer) error
+}
+
+// SkPipe is Struct of SkPipeIf
+type SkPipe struct {
+	skExecAr []SkExecIf
+}
+
+// AddExec pipeで実行する処理を追加する
+func (sp SkPipe) AddExec(skexec SkExecIf) {
+	if sp.skExecAr == nil {
+		sp.skExecAr = make([]SkExecIf, 0, 5)
+	}
+	sp.skExecAr = append(sp.skExecAr, skexec)
 }
