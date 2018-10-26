040fbe45f58064775af05b5cfeb821b0710ff8c2
DaishinUehara <git.uehara@gmail.com>
2018-10-26 16:20:52 +0900

    Add Error Stack Print Function.

---
+++ skerrlib/skcalldumplib.go
@@ -0,0 +1,57 @@
+package skerrlib
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"runtime"
+)
+
+// SkCallInfo コール情報を格納する構造体
+type SkCallInfo struct {
+	PkgName  string
+	FuncName string
+	FileName string
+	FileLine int
+}
+
+// パッケージ名と関数名を分離する正規表現
+var regPkgFunc = regexp.MustCompile(`^(\S.+)\.(\S.+)$`)
+
+// SkCallStack コール情報をダンプしコールスタックとして格納する。
+func SkCallStack(i int) (callstack []*SkCallInfo) {
+	for ; ; i++ {
+		pc, _, _, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+
+		fn := runtime.FuncForPC(pc)
+		fileName, fileLine := fn.FileLine(pc)
+		_fn := regPkgFunc.FindStringSubmatch(fn.Name())
+		callstack = append(callstack, &SkCallInfo{
+			PkgName:  _fn[1],
+			FuncName: _fn[2],
+			FileName: fileName,
+			FileLine: fileLine,
+		})
+	}
+	return
+}
+
+// PrintCallStack is reutrn CallStackString
+func PrintCallStack() string {
+	callstack := SkCallStack(2)
+	var ret = bytes.NewBuffer(make([]byte, 0, 100))
+	for _, callinfo := range callstack {
+		ret.WriteString("[filename=")
+		ret.WriteString(callinfo.FileName)
+		ret.WriteString("][line=")
+		ret.WriteString(fmt.Sprintf("%d", callinfo.FileLine))
+		ret.WriteString("]")
+		ret.WriteString(callinfo.PkgName)
+		ret.WriteString(".")
+		ret.WriteString(callinfo.FuncName)
+	}
+	return ret.String()
+}
