05a7e1d9e2dd57f3fbe722ece2ff76973def4fbf
DaishinUehara <git.uehara@gmail.com>
2018-11-07 01:31:41 +0900

    Add Rotation Log.

--- skloglib/skloglib.go
+++ skloglib/skloglib.go
@@ -2,65 +2,90 @@ package skloglib
 
 import (
 	"io/ioutil"
-
 	"path/filepath"
 
 	"github.com/DaishinUehara/suikin/skconflib"
 	"github.com/DaishinUehara/suikin/skerrlib"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
-	//	"github.com/natefinch/lumberjack"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
 	yaml "gopkg.in/yaml.v2"
 )
 
 const (
-	defaultConfFile = "../conf/log.yml"
+	defaultLogFormatConf   = "../conf/log_format.yml"
+	defalutLogRotationConf = "../conf/log_rotation.yml"
 )
 
 // SkLogger is Suikin Logger.
 type SkLogger struct {
 	logger    *zap.Logger
 	logconfig zap.Config
+	lmblogger *lumberjack.Logger
 }
 
 var conf skconflib.SkConf
 
 // GetLogger is get logger.
 func (skl *SkLogger) GetLogger() (*zap.Logger, error) {
-	var conffile string
+	var formatconf string
 	var err error
-	var fpath string
+	var rotationconf string
 	err = nil
-	skl.logger, err = zap.NewProduction()
-	if err != nil {
-		// zap loggerの取得に失敗した場合
-		return nil, skerrlib.ErrLoggerGenerate{Err: err, StackTrace: skerrlib.PrintCallStack()}
-	}
-	conffile = conf.GetLogConfig("CONFIG_YAML")
-	if conffile == "" {
-		conffile = defaultConfFile
-	}
-	fpath, err = filepath.Abs(conffile)
-	if err != nil {
-		// 設定ファイルの絶対パス取得に失敗した場合
-		return nil, skerrlib.ErrGetAbsolutePath{Err: err, StackTrace: skerrlib.PrintCallStack()}
-	}
+	/*
+		skl.logger, err = zap.NewProduction()
+		if err != nil {
+			// zap loggerの取得に失敗した場合
+			return nil, skerrlib.ErrLoggerGenerate{Err: err, StackTrace: skerrlib.PrintCallStack()}
+		}
+	*/
 
-	configYaml, err := ioutil.ReadFile(fpath)
+	formatconf = conf.GetLogConfig("FORMAT_YAML")
+	if formatconf == "" {
+		formatconf = defaultLogFormatConf
+	}
+	configYaml, err := ReadByteFile(formatconf)
+	if err = yaml.Unmarshal(configYaml, &skl.logconfig); err != nil {
+		// 設定ファイルを構造体にセットできなかった場合
+		return nil, skerrlib.ErrYamlMapping{Err: err, StackTrace: skerrlib.PrintCallStack()}
+	}
+	enc := zapcore.NewJSONEncoder(skl.logconfig.EncoderConfig)
+
+	rotationconf = conf.GetLogConfig("ROTATION_YAML")
+	if rotationconf == "" {
+		rotationconf = defalutLogRotationConf
+	}
+	rotateYaml, err := ReadByteFile(rotationconf)
 	if err != nil {
 		// 設定ファイルの読み込みに失敗した場合
 		return nil, skerrlib.ErrReadFile{Err: err, StackTrace: skerrlib.PrintCallStack()}
 	}
-	logconfig := &skl.logconfig
-	if err = yaml.Unmarshal(configYaml, logconfig); err != nil {
+
+	if err = yaml.Unmarshal(rotateYaml, &skl.lmblogger); err != nil {
 		// 設定ファイルを構造体にセットできなかった場合
 		return nil, skerrlib.ErrYamlMapping{Err: err, StackTrace: skerrlib.PrintCallStack()}
 	}
-	skl.logger, err = logconfig.Build()
-	if err != nil {
-		// ロガーのビルドに失敗した場合
-		return nil, err
-	}
+
+	sink := zapcore.AddSync(skl.lmblogger)
+
+	skl.logger = zap.New(zapcore.NewCore(enc, sink, skl.logconfig.Level))
+
 	return skl.logger, err
 
+}
+
+// ReadByteFile is ReadFile absolute path or relative path.
+func ReadByteFile(path string) ([]byte, error) {
+	rconf, err := filepath.Abs(path)
+	if err != nil {
+		// 設定ファイルの絶対パス取得に失敗した場合
+		return nil, skerrlib.ErrGetAbsolutePath{Err: err, StackTrace: skerrlib.PrintCallStack()}
+	}
+	data, err := ioutil.ReadFile(rconf)
+	if err != nil {
+		// 設定ファイルの読み込みに失敗した場合
+		return nil, skerrlib.ErrReadFile{Err: err, StackTrace: skerrlib.PrintCallStack()}
+	}
+	return data, err
 }
--- skloglib/skloglib_test.go
+++ skloglib/skloglib_test.go
@@ -1,9 +1,6 @@
 package skloglib_test
 
 import (
-	"bytes"
-	"io"
-	"os"
 	"testing"
 	"time"
 
@@ -14,21 +11,21 @@ import (
 var loglib skloglib.SkLogger
 
 func TestGetLogger(t *testing.T) {
+	//	stdout := os.Stdout // backup os.Stdout
+	//	r, w, _ := os.Pipe()
+	//	os.Stdout = w // connect
 	logger, err := loglib.GetLogger()
 	if err != nil {
 		t.Errorf("[NG]:err=%v\n", err)
 	} else {
 		defer logger.Sync()
 		arr := []string{"abc", "def", "hij"}
-		stdout := os.Stdout // backup os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w // connect
 		logger.Info("Hello zap", zap.String("key", "value"), zap.Time("now", time.Now()), zap.Strings("stack", arr))
-		os.Stdout = stdout
-		w.Close()
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		str := buf.String()
-		t.Logf("[OK]:%s\n", str)
+		//		os.Stdout = stdout
+		//		w.Close()
+		//		var buf bytes.Buffer
+		//		io.Copy(&buf, r)
+		//		str := buf.String()
+		//		t.Logf("[OK]:%s\n", str)
 	}
 }
