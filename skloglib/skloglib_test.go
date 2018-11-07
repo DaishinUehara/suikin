b89f873cb9f74021d99b02b77dffe1b90e02fac6
DaishinUehara <git.uehara@gmail.com>
2018-11-07 22:48:10 +0900

    Add test code.

--- skloglib/skloglib_test.go
+++ skloglib/skloglib_test.go
@@ -52,8 +52,10 @@ func TestGetLogger(t *testing.T) {
 			// 設定ファイルを構造体にセットできなかった場合
 			t.Errorf("[NG]:err=%v\n", err)
 		} else {
-			if logd.Key == "value" {
-				t.Logf("[OK]:key=%v\n", logd.Key)
+			if logd.Level == "INFO" && logd.Msg == "Hello zap" && logd.Key == "value" && logd.Stack[0] == "abc" && logd.Stack[1] == "def" && logd.Stack[2] == "hij" {
+				t.Logf("[OK]:Level=%s,Msg=%s,key=%s,logd.Stack=%v\n", logd.Level, logd.Msg, logd.Key, logd.Stack)
+			} else {
+				t.Errorf("[NG]:Level=%s,Msg=%s,key=%s,logd.Stack=%v\n", logd.Level, logd.Msg, logd.Key, logd.Stack)
 			}
 		}
 
