9ab688a79069973c488c5f255353e0554ae86e6e
DaishinUehara <git.uehara@gmail.com>
2018-11-11 01:15:27 +0900

    Add Function LogOutput To ErrFlushBuffer.

--- skerrloglib/skerrloglib.go
+++ skerrloglib/skerrloglib.go
@@ -18,3 +18,29 @@ func (e *ErrOutOfIndex) LogOutput() error {
 	logger.Error("Out of Index Error.", zap.String("ArrayName", e.ArrayName), zap.Int("Index", e.Index), zap.Strings("StackTrace", e.StackTrace))
 	return err
 }
+
+// ErrNotInitialized Error out of index.
+type ErrNotInitialized skerrlib.ErrNotInitialized
+
+// LogOutput Output Log "Index Error"
+func (e *ErrNotInitialized) LogOutput() error {
+	logger, err := skloglib.SkLog.GetLogger()
+	if err != nil {
+		return err
+	}
+	logger.Error("Not Initialized Error.", zap.String("NoInitializedItem", e.NoInitializedItem), zap.Strings("StackTrace", e.StackTrace))
+	return err
+}
+
+// ErrFlushBuffer Flush Buffer Error.
+type ErrFlushBuffer skerrlib.ErrFlushBuffer
+
+// LogOutput Output Log "Flush Buffer Error"
+func (e *ErrFlushBuffer) LogOutput() error {
+	logger, err := skloglib.SkLog.GetLogger()
+	if err != nil {
+		return err
+	}
+	logger.Error("Flush Buffer Error.", zap.String("ErrorItem", e.ErrorItem), zap.Strings("StackTrace", e.StackTrace))
+	return err
+}
